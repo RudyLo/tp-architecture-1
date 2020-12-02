71aa402c21f813a0963c097f23dd3f9b1e636437
armelito60 <tchiasso@et.esiea.fr>
2020-12-02 15:15:26 +0100

    première page web "hello world" créée pour test de bon fonctionnement

--- src/main/java/com/progdistribuee/springboot/Application.java
+++ src/main/java/com/progdistribuee/springboot/Application.java
@@ -2,12 +2,13 @@ package com.progdistribuee.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
